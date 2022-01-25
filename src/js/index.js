3bb9d87205d52c5969d6aa316bcd862ca0af7073
JulienRouet <julienrouet@live.fr>
2022-01-25 14:19:18 +0100

    first

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+import '../style/index.scss'; // Attention ici, il faut bien mettre l'extension `.scss`
+import 'bootstrap';
+
+// Alternatively, you may import plugins individually as needed:
+
+// Copy
+// import 'bootstrap/js/dist/util';
+// import 'bootstrap/js/dist/dropdown';
+// ...
+
+console.log("Hello!");
